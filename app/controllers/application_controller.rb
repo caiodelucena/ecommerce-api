29670840e15fc67ec18493ac52534b0a83510370
Caio Lucena <caiodelucena6@gmail.com>
2023-03-23 09:12:52 -0300

    feat: add more parameters on signup as name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+  end
 end
