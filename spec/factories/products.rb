391cbd226a9119de740c80f13068cf5639958d9f
Caio Lucena <caiodelucena6@gmail.com>
2023-05-31 21:34:16 -0300

    test: build game to product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,6 +3,9 @@ FactoryBot.define do
     name { Faker::Commerce.product_name }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 100.0..400.0) }
-    productable { nil }
+
+    after :build do |product|
+      product.productable = create(:game)
+    end
   end
 end
